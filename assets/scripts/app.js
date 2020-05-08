3201efeb3f2c5d6731f710d5b3e0cb07565b82e5
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-08 23:50:12 +0530

    attackHandler function modified to include draw condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,10 +13,12 @@ function attackHandler() {
     currentMonsterHealth -= damage;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
-    if (currentMonsterHealth <= 0 ) {
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0 ) {
         alert('You Won!!!!');
-    } else if (currentPlayerHealth <= 0) {
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth >0 ) {
         alert('You Lost!!!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0 ) {
+        alert('You have a draw!!!');
     }
 }
 
