206ea0a7962d9178015b42357fd7931f7268103d
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-09 19:42:52 +0530

    writeToLog using switch statement to evaludate event condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -35,16 +35,33 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
         finalPlayerHealth : playerHealth
     }
 
-    if (event === LOG_EVENT_PLAYER_ATTACK) {
-        logEntry.target = 'MONSTER';
-    } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-        logEntry.target = 'MONSTER';
-    } else if (event === LOG_EVENT_MONSTER_ATTACK) {
-        logEntry.target = 'PLAYER';
-    } else if (event === LOG_EVENT_PLAYER_HEAL) {
-        logEntry.target = 'PLAYER';
-    }  else if (event === LOG_EVENT_GAME_OVER) {
+    switch(event) {
+        case LOG_EVENT_PLAYER_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case  LOG_EVENT_MONSTER_ATTACK:
+            logEntry.target = 'PLAYER';
+            break;
+        case LOG_EVENT_PLAYER_HEAL:
+            logEntry.target = 'PLAYER';
+            break;
+        case LOG_EVENT_GAME_OVER:
+            break;
     }
+
+    // if (event === LOG_EVENT_PLAYER_ATTACK) {
+    //     logEntry.target = 'MONSTER';
+    // } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+    //     logEntry.target = 'MONSTER';
+    // } else if (event === LOG_EVENT_MONSTER_ATTACK) {
+    //     logEntry.target = 'PLAYER';
+    // } else if (event === LOG_EVENT_PLAYER_HEAL) {
+    //     logEntry.target = 'PLAYER';
+    // }  else if (event === LOG_EVENT_GAME_OVER) {
+    // }
     battleLog.push(logEntry);
 }
 
